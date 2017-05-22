7950766cf94ee19e721c0e69faf53871366f894a
DanVev <vasily.danilin@yandex.ru>
2017-05-22 12:40:02 +0300

    Test by hidden neuron number

--- two-layer-network.py
+++ two-layer-network.py
@@ -6,10 +6,6 @@ IMAGE_SHAPE = (28, 28)
 HIDDEN_LAYER_NEURON_NUMBER = 20
 TRAINING_SPEED = 0.50
 EPOCH_NUMBER = 1
-w1 = (2 * np.random.rand(HIDDEN_LAYER_NEURON_NUMBER, 784) - 1) / 10
-w2 = (2 * np.random.rand(10, HIDDEN_LAYER_NEURON_NUMBER) - 1) / 10
-b1 = (2 * np.random.rand(HIDDEN_LAYER_NEURON_NUMBER) - 1) / 10
-b2 = (2 * np.random.rand(10) - 1) / 10
 
 print("Load data from MNIST database")
 mndata = MNIST(r"D:\Projects\PycharmProjects\Perceptron\mnist")
@@ -33,13 +29,18 @@ for i in range(0, len(tr_images)):
 print("Normalization is completed")
 
 print("Start of training")
-for epoch_number in range(EPOCH_NUMBER):
+for neuron_number in range(5, 250, 3):
     print("--------------------------------------")
-    print("Epoch number #", epoch_number + 1)
-    TRAINING_SPEED -= 0.05
+    print("Neuron number in hidden layer #", neuron_number)
+    HIDDEN_LAYER_NEURON_NUMBER = neuron_number
+    w1 = (2 * np.random.rand(HIDDEN_LAYER_NEURON_NUMBER, 784) - 1) / 10
+    w2 = (2 * np.random.rand(10, HIDDEN_LAYER_NEURON_NUMBER) - 1) / 10
+    b1 = (2 * np.random.rand(HIDDEN_LAYER_NEURON_NUMBER) - 1) / 10
+    b2 = (2 * np.random.rand(10) - 1) / 10
+    # TRAINING_SPEED -= 0.05
     for n in range(len(tr_images)):
-        if n % 10000 == 0:
-            print("Training number:", n)
+        # if n % 10000 == 0:
+        #    print("Training number:", n)
         img = tr_images[n]
         cls = tr_labels[n]
 
@@ -81,37 +82,37 @@ for epoch_number in range(EPOCH_NUMBER):
         b2 += TRAINING_SPEED * delta2
         b1 += TRAINING_SPEED * delta1
 
-
-def nn_calculate(img):
-    resp1 = list(range(0, HIDDEN_LAYER_NEURON_NUMBER))
-    resp2 = list(range(0, 10))
-    # first layer
-    for i in range(0, HIDDEN_LAYER_NEURON_NUMBER):
-        r = w1[i] * img
-        r = sigmoid(np.sum(r) + b1[i])
-        resp1[i] = r
-
-    # second layer
-    for i in range(0, 10):
-        r = w2[i] * resp1
-        r = sigmoid(np.sum(r) + b2[i])
-        resp2[i] = r
-    return np.argmax(resp2)
+    total = len(test_images)
+    valid = 0
+    invalid = []
 
 
-total = len(test_images)
-valid = 0
-invalid = []
+    def nn_calculate(img):
+        resp1 = list(range(0, HIDDEN_LAYER_NEURON_NUMBER))
+        resp2 = list(range(0, 10))
+        # first layer
+        for i in range(0, HIDDEN_LAYER_NEURON_NUMBER):
+            r = w1[i] * img
+            r = sigmoid(np.sum(r) + b1[i])
+            resp1[i] = r
 
-for i in range(0, total):
-    img = test_images[i]
-    predicted = nn_calculate(img)
-    true = test_labels[i]
-    if predicted == true:
-        valid += 1
-    else:
-        invalid.append({"image": img, "predicted": predicted, "true": true})
+        # second layer
+        for i in range(0, 10):
+            r = w2[i] * resp1
+            r = sigmoid(np.sum(r) + b2[i])
+            resp2[i] = r
+        return np.argmax(resp2)
 
-print("--------------------------------------")
-print("accuracy {}".format(valid / total))
-# print(invalid[:50])
+
+    for i in range(0, total):
+        img = test_images[i]
+        predicted = nn_calculate(img)
+        true = test_labels[i]
+        if predicted == true:
+            valid += 1
+        else:
+            invalid.append({"image": img, "predicted": predicted, "true": true})
+
+    # print("--------------------------------------")
+    print("Accuracy {}%".format(valid / total * 100))
+    # print(invalid[:50])
